1779c43646386a4c84feffb7b6c7f762c3c679aa
Patric Eberle <line-in@derverstaerker.ch>
2018-01-19 18:35:24 +0100

    Fixes build size

--- app/main.js
+++ app/main.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import plugins from './setup/plugins';
 import options from './setup/options';
-import devOptions from './setup/options.dev'; // Webpack will ignore imports which are not used
 import components from './setup/components';
 
 let vueOptions = options;
@@ -12,7 +11,8 @@ Vue.use(components);
 Vue.use(plugins);
 
 if (!WP_PRODUCTION || WP_STYLEGUIDE) {
-  vueOptions = Object.assign(vueOptions, devOptions); // Note: will overwrite duplicates
+  // eslint-disable-next-line global-require
+  vueOptions = Object.assign(vueOptions, require('./setup/options.dev')); // Note: will overwrite duplicates
 
   // eslint-disable-next-line global-require
   // require('./setup/mock').default(plugins.vueAxios.axios, developmentOptions);
