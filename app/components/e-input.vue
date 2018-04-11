e3c98c730b6a156708a68ff3adf2d2c8180551d4
Silvan Baumann <silvan.baumann@cec.valantic.com>
2018-04-11 14:26:38 +0200

    WF-447: add e-input component

---
+++ app/components/e-input.vue
@@ -0,0 +1,39 @@
+<template>
+  <div :class="b()">
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'e-input',
+    // components: {},
+    // mixins: [],
+
+    // props: {},
+    // data() {
+    //   return {};
+    // },
+
+    // computed: {},
+    // watch: {},
+
+    // beforeCreate() {},
+    // created() {},
+    // beforeMount() {},
+    // mounted() {},
+    // beforeUpdate() {},
+    // updated() {},
+    // activated() {},
+    // deactivated() {},
+    // beforeDestroy() {},
+    // destroyed() {},
+
+    // methods: {},
+    // render() {},
+  };
+</script>
+
+<style lang="scss">
+  .e-input {}
+</style>
