8e13f15864480850a86661bc06784c4bb8846a1b
Silvan Baumann <silvan.baumann@cec.valantic.com>
2018-04-12 18:41:11 +0200

    WF-447: adds styling for e-input component and states

---
+++ app/components/e-input.md
@@ -0,0 +1,49 @@
+### e-input
+
+#### `default`
+```
+<e-input name="demo" placeholder="enter text..."/>
+```
+
+
+#### `:disabled`
+```
+<e-input disabled name="demo" value="Lorem ipsum..."/>
+```
+
+
+#### `:hover`
+```
+<e-input name="demo" placeholder="..."/>
+```
+
+
+#### `:active`
+```
+<e-input name="demo" placeholder="..."/>
+```
+
+#### `:focus`
+```
+<e-input name="demo" placeholder="..."/>
+```
+
+#### state: `error`
+```
+<e-input state="error" name="demo" placeholder="..."/>
+```
+
+#### state: `success`
+```
+<e-input state="success" name="demo" placeholder="..."/>
+```
+
+#### state: `info`
+```
+<e-input state="info" name="demo" placeholder="..."/>
+```
+
+
+
+
+<!-- <e-input value="foo" @input="actionName" state="active" name="demo" type="text" placeholder="..."></e-input> -->
--- app/components/e-input.vue
+++ app/components/e-input.vue
@@ -1,7 +1,14 @@
-<template>
-  <div :class="b()">
+<!-- This component has no <template> because of dynamic root element -->
 
+<template>
+
+  <div :class="b({ state })">
+    <input :class="b('field')"
+           :disabled="$attrs.disabled"
+           :placeholder="$attrs.placeholder"
+           :value="$attrs.value">
   </div>
+
 </template>
 
 <script>
@@ -10,12 +17,44 @@
     // components: {},
     // mixins: [],
 
-    // props: {},
+    props: {
+
+      /**
+       * User input
+       */
+      input: {
+        default: null,
+        required: false,
+        type: String
+      },
+
+      // value
+
+      /**
+       * Adds the state to the input field
+       */
+      state: {
+        default: null,
+        required: false,
+        type: String,
+        validator(value) {
+          return [
+            'error',
+            'info',
+            'success',
+          ].includes(value);
+        }
+      }
+    },
+
+    // computed: {
+    //   return {}
+    // },
+
     // data() {
     //   return {};
     // },
 
-    // computed: {},
     // watch: {},
 
     // beforeCreate() {},
@@ -35,5 +74,86 @@
 </script>
 
 <style lang="scss">
-  .e-input {}
+  .e-input {
+    position: relative;
+
+    &::before,
+    &::after {
+      content: "";
+      display: block;
+      height: 55%;
+      left: 0;
+      position: absolute;
+      z-index: 1;
+      top: 20%;
+      width: 100%;
+      @include media(sm) {
+        width: 151px;
+      }
+    }
+
+    &::before {
+      border-left: 1px solid $color-primary--3;
+    }
+
+    &::after {
+      border-right: 1px solid $color-primary--3;
+    }
+
+    &__field {
+      border: 1px solid $color-grayscale--300;
+      border-radius: 2px;
+      color: $color-grayscale--400;
+      font-family: $font-family--primary;
+      font-size: $font-size--14;
+      height: 18px;
+      line-height: 18px;
+      position: relative;
+      width: 100%;
+      padding: $spacing--5 $spacing--10;
+
+      @include media(sm) {
+        width: 130px;
+      }
+
+      &::placeholder {
+        color: $color-grayscale--400;
+      }
+
+      &:disabled {
+        border-color: $color-grayscale--600;
+        color: $color-grayscale--600;
+      }
+
+      &:hover {
+        // full border...
+      }
+
+      &:active,
+      &:focus {
+        box-shadow: 0 2px 5px -2px rgba($color-grayscale--400, 0.75);
+        color: $color-secondary--1;
+        outline: none;
+      }
+    }
+  }
+
+  /**
+  * states
+  **/
+
+  // error
+  .e-input--state-error .e-input__field {
+    border-color: $color-secondary--4;
+  }
+
+  // info
+  .e-input--state-info .e-input__field {
+
+  }
+
+  // success
+  .e-input--state-success .e-input__field {
+
+  }
 </style>
