7f67069f9567b56c2c1b75dfca8623ecc591a46f
Lukas Böhler <lukas.boehler@cec.valantic.com>
2018-04-19 17:27:02 +0200

    Add a Heading Element

---
+++ app/components/e-heading.md
@@ -0,0 +1,51 @@
+### Different Tags
+
+```
+<div>
+  <e-heading level="h1">Headline-1 Regular (components Title)</e-heading>
+  <e-heading level="h2">Headline-2 (Information Title)</e-heading>
+  <e-heading level="h3">Headline-3 (Information Title)</e-heading>
+  <e-heading level="h4">Headline-4 (Navigation Title aktive)</e-heading>
+  <e-heading level="h5">Headline-5 (Button)</e-heading>
+  <a href="#"><e-heading level="h6">Headline-6 (Link)</e-heading></a>
+</div>
+```
+
+### Alternate color
+
+```
+<div>
+  <e-heading level="h1" color="darkgrey">Headline-1 Regular (components Title)</e-heading>
+  <e-heading level="h2" color="green">Headline-2 (Information Title)</e-heading>
+  <e-heading level="h3" color="darkgrey">Headline-3 (Information Title)</e-heading>
+  <e-heading level="h4" color="green">Headline-4 (Navigation Title aktive)</e-heading>
+  <e-heading level="h5" color="blue">Headline-5 (Button)</e-heading>
+  <a href="#"><e-heading level="h6" color="green">Headline-6 (Link)</e-heading></a>
+</div>
+```
+
+### With underline
+
+```
+<div>
+  <e-heading level="h1" underline="true">Headline-1 Regular (components Title)</e-heading>
+  <e-heading level="h2" underline="true">Headline-2 (Information Title)</e-heading>
+  <e-heading level="h3" underline="true">Headline-3 (Information Title)</e-heading>
+  <e-heading level="h4" underline="true">Headline-4 (Navigation Title aktive)</e-heading>
+  <e-heading level="h5" underline="true">Headline-5 (Button)</e-heading>
+  <a href="#"><e-heading level="h6" underline="true">Headline-6 (Link)</e-heading></a>
+</div>
+```
+
+### Alternate font weight
+
+```
+<div>
+  <e-heading level="h1" weight="bold">Headline-1 Regular (components Title)</e-heading>
+  <e-heading level="h2" weight="semibold">Headline-2 (Information Title)</e-heading>
+  <e-heading level="h3" weight="bold">Headline-3 (Information Title)</e-heading>
+  <e-heading level="h4" weight="normal">Headline-4 (Navigation Title aktive)</e-heading>
+  <e-heading level="h5" weight="normal">Headline-5 (Button)</e-heading>
+  <a href="#"><e-heading level="h6" weight="bold">Headline-6 (Link)</e-heading></a>
+</div>
+```
---
+++ app/components/e-heading.vue
@@ -0,0 +1,164 @@
+<!-- This component has no <template> because of dynamic root element -->
+
+<script>
+  export default {
+    name: 'e-heading',
+    // components: {},
+    // mixins: [],
+
+    props: {
+      /**
+       * Defines the color of the heading (h1-h6)
+       */
+      color: {
+        type: String,
+        default: null,
+        validator(value) {
+          return [
+            'blue',
+            'green',
+            'darkgrey',
+          ].includes(value);
+        },
+      },
+
+      /**
+       * Defines the level of the heading (h1-h6)
+       */
+      level: {
+        type: String,
+        required: true,
+      },
+
+      /**
+       * Defines if the heading should have an underline
+       */
+      underline: {
+        type: Boolean,
+        default: null,
+      },
+
+      /**
+       * Defines the font weight
+       */
+      weight: {
+        type: String,
+        default: null,
+        validator(value) {
+          return [
+            'normal',
+            'semibold',
+            'bold',
+          ].includes(value);
+        },
+      },
+    },
+
+    // computed: {},
+    // watch: {},
+
+    // beforeCreate() {},
+    // created() {},
+    // beforeMount() {},
+    // mounted() {},
+    // beforeUpdate() {},
+    // updated() {},
+    // activated() {},
+    // deactivated() {},
+    // beforeDestroy() {},
+    // destroyed() {},
+
+    // methods: {},
+
+    /**
+     * Creates a button or button like link based on defined/missing href link
+     *
+     * @param   {function}    createElement   Vue helper
+     *
+     * @returns {*}
+     */
+    render(createElement) {
+      const element = this.$props.level;
+      const attributes = {
+        class: this.b({
+          color: this.$props.color,
+          underline: this.$props.underline === 'true',
+          weight: this.$props.weight,
+        }),
+        attrs: {
+
+        },
+      };
+
+      return createElement(
+        element,
+        attributes,
+        this.$slots.default,
+      );
+    },
+  };
+</script>
+
+<style lang="scss">
+  .e-heading {
+    font-family: "Source Sans Pro", sans-serif;
+    font-weight: normal;
+    color: #3a77ad;
+    letter-spacing: 2.03px;
+    line-height: 23px;
+
+    &.e-heading--weight-normal {
+      font-weight: normal;
+    }
+
+    &.e-heading--weight-semibold {
+      font-weight: 600;
+    }
+
+    &.e-heading--weight-bold {
+      font-weight: bold;
+    }
+
+    &.e-heading--underline {
+      text-decoration: underline;
+    }
+
+    &.e-heading--color-blue {
+      color: $color-secondary--1;
+    }
+
+    &.e-heading--color-darkgrey {
+      color: $color-grayscale--200;
+    }
+
+    &.e-heading--color-green {
+      color: $color-secondary--3;
+    }
+  }
+
+  h1.e-heading {
+    font-size: 18px;
+  }
+
+  h2.e-heading {
+    font-size: 16px;
+  }
+
+  h3.e-heading {
+    font-size: 14px;
+  }
+
+  h4.e-heading {
+    font-size: 14px;
+    font-weight: bold;
+  }
+
+  h5.e-heading {
+    font-size: 14px;
+    font-weight: 600;
+  }
+
+  h6.e-heading {
+    font-size: 14px;
+  }
+</style>
