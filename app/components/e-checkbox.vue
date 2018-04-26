6fa1bb277ea5a904402f082fe4599780061804ef
Silvan Baumann <silvan.baumann@cec.valantic.com>
2018-04-26 10:03:21 +0200

    WF-450: - creates e-checkbox component

---
+++ app/components/e-checkbox.vue
@@ -0,0 +1,44 @@
+<template>
+  <div :class="b()">
+
+    <input v-model="checked" type="checkbox"> {{ checked }}
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'e-checkbox',
+    // components: {},
+    // mixins: [],
+
+    // props: {},
+    data() {
+      return {
+        checked: false
+      };
+    },
+
+    // computed: {},
+    // watch: {},
+
+    // beforeCreate() {},
+    // created() {},
+    // beforeMount() {},
+    // mounted() {},
+    // beforeUpdate() {},
+    // updated() {},
+    // activated() {},
+    // deactivated() {},
+    // beforeDestroy() {},
+    // destroyed() {},
+
+    // methods: {},
+    // render() {},
+  };
+</script>
+
+<style lang="scss">
+  .e-checkbox {
+  }
+</style>
