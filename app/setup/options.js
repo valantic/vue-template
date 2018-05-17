11b696fbb8eb0ca177fa5b45abda2cd75191355d
Patric Eberle <patric.eberle@cec.valantic.com>
2018-05-17 12:35:24 +0200

    #WF-627: simplifies file/folder setup

--- app/setup/options.js
+++ app/setup/options.js
@@ -1,20 +1,5 @@
-import Vue from 'vue';
 import store from '@/store/index';
-import VueAxios from '@/setup/plugins/axios';
-import VueBemCn from 'vue-bem-cn';
-
-// Polyfills and self executing
-import 'intersection-observer';
-import 'picturefill';
-import 'lazysizes';
-import './lib/ls.attrchange'; // lazy sizes attribute change observer
-
 import { i18n } from './i18n';
-
-Vue.use(VueAxios);
-Vue.use(VueBemCn, {
-  hyphenate: true,
-});
 
 export default {
   el: '#app',
--- app/setup/styleguidist/required.js
+++ app/setup/styleguidist/required.js
@@ -1,17 +1,8 @@
 import Vue from 'vue';
-import VueBemCn from 'vue-bem-cn';
-import VueAxios from '@/setup/plugins/axios';
 import components from '@/setup/components';
 
 // Polyfills and self executing
-import 'intersection-observer';
-import 'picturefill';
-import 'lazysizes';
-import '@/setup/lib/ls.attrchange'; // lazy sizes attribute change observer
 import '@/setup/styleguide.options';
+import '@/setup/plugins';
 
 Vue.use(components);
-Vue.use(VueAxios);
-Vue.use(VueBemCn, {
-  hyphenate: true,
-});
