18358c2e56bfcd567bcff0871ac48e8cf3a5c2bb
Claudio Schäpper <claudio.schaepper@cec.valantic.com>
2018-05-22 09:13:07 +0200

    WF-581: sets `allChunks` of ExtractTextPlugin to false so the style is added as a file to the project and not head styles

--- webpack.config.js
+++ webpack.config.js
@@ -118,11 +118,7 @@ module.exports = function(env = {}, options = {}) {
       // extract css into its own file
       new ExtractTextPlugin({
         filename: assetsSubDirectory + `css/${prefix}[name].css?[chunkhash]`, // NOTE: postcss-pipeline currently does not support query hash (https://github.com/mistakster/postcss-pipeline-webpack-plugin/issues/30)
-        // Setting the following option to `false` will not extract CSS from codesplit chunks.
-        // Their CSS will instead be inserted dynamically with style-loader when the codesplit chunk has been loaded by webpack.
-        // It's currently set to `true` because we are seeing that sourcemaps are included in the codesplit bundle as well when it's `false`,
-        // increasing file size: https://github.com/vuejs-templates/webpack/issues/1110
-        allChunks: true,
+        allChunks: false,
       })
     ];
 
