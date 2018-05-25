4018d46b5a840ae2facab39f8c0914a1e9c4fbd4
Adrian Burtscher <adrian.burtscher@antiloop.com>
2018-05-25 11:00:10 +0200

    WF-585_c-productlist-wrapper removed unnecessary function 'fetchProduct' in store/modules/catalog/index.js

--- app/store/modules/catalog/index.js
+++ app/store/modules/catalog/index.js
@@ -1,6 +1,4 @@
 import productListData from '@/store/modules/catalog/mock/productlist';
-import productData from '@/store/modules/product/mock/product';
-import api from "@/store/utils/api";
 
 export default {
   namespaced: true,
@@ -9,7 +7,6 @@ export default {
      * @property {Object}   Stores productlist data
      */
     productlist: productListData,
-    product: productData,
   },
   getters: {
     /**
@@ -20,29 +17,8 @@ export default {
      * @returns  {Object}  product   Productlist data
      */
     productlist: state => state.productlist,
-
-    product: state => state.product,
   },
   mutations: {},
-  actions: {
-    /**
-     *
-     * @param state
-     * @param commit
-     * @returns {Promise<T>}
-     */
-    fetchProduct({ state, commit }) {
-      return api.post('/product/multi-get', { sku: state.product.sku, quantity: 1 })
-        .then((response) => {
-          if (response && response.data && response.data.products) {
-            commit('setProduct', response.data[0]);
-
-            return response;
-          }
-
-          throw new Error('apiFailure');
-      });
-    },
-  },
+  actions: {},
 };
 
