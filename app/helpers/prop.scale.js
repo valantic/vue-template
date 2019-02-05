c010baa805a5a530f1abe3673ffe219b4fd6ada1
Patric Eberle <patric.eberle@cec.valantic.com>
2019-02-05 13:40:11 +0100

    #64 makes validator method of props dynamic since it is not used in production mode anyway.

--- app/helpers/prop.scale.js
+++ app/helpers/prop.scale.js
@@ -7,17 +7,20 @@
  * @returns {Object}
  */
 export default function(validNumbers, defaultValue = 500) {
+  const propConfig = {
+    type: [Number, String],
+    default: defaultValue
+  };
+
   if (process.env.NODE_ENV !== 'production') {
     if (!Array.isArray(validNumbers)) {
       throw new Error("'validNumbers' is not an array.");
     }
+
+    propConfig.validator = function(value) { // Note, that validators are not triggered in production mode.
+      return validNumbers.includes(parseInt(value, 10));
+    };
   }
 
-  return {
-    type: [Number, String],
-    default: defaultValue,
-    validator(value) {
-      return validNumbers.includes(parseInt(value, 10));
-    }
-  };
+  return propConfig;
 }
