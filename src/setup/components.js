5f360b66310a11529bdf1d3f16707a4aa1d304ab
Patric Eberle <line-in@derverstaerker.ch>
2019-06-16 18:06:57 +0200

    Version 4.0: changes order of global component imports.

--- src/setup/components.js
+++ src/setup/components.js
@@ -1,16 +1,18 @@
+// Form elements
+import eLabel from '@/components/e-label';
+import eInput from '@/components/e-input';
+import eSelect from '@/components/e-select';
+import eRadio from '@/components/e-radio';
+import eCheckbox from '@/components/e-checkbox';
+import eTextarea from '@/components/e-textarea';
+import eButton from '@/components/e-button';
+
 // Elements
 import eIcon from '@/components/e-icon';
-import eButton from '@/components/e-button';
 import ePicture from '@/components/e-picture';
 import eHeading from '@/components/e-heading';
 import ePanel from '@/components/c-panel';
-import eInput from '@/components/e-input';
-import eCheckbox from '@/components/e-checkbox';
-import eLabel from '@/components/e-label';
 import eProgress from '@/components/e-progress';
-import eTextarea from '@/components/e-textarea';
-import eRadio from '@/components/e-radio';
-import eSelect from '@/components/e-select';
 import eLink from '@/components/e-link';
 
 // Components
@@ -27,20 +29,20 @@ require.context('@/components', false, /\.scss/);
 export default {
   install(Vue) {
     const components = [
+      eLabel,
+      eInput,
+      eSelect,
+      eRadio,
+      eCheckbox,
+      eTextarea,
       eButton,
       eIcon,
       ePicture,
       eHeading,
-      eInput,
-      eCheckbox,
-      eLabel,
       eProgress,
-      eTextarea,
-      eRadio,
       ePanel,
       cModal,
       cModalHeader01,
-      eSelect,
       lDefault,
       eLink,
       cBreadcrumb,
@@ -52,20 +54,20 @@ export default {
         Vue.component(component.name, component);
       });
     } else {
+      Vue.component(eLabel.name, eLabel);
+      Vue.component(eInput.name, eInput);
+      Vue.component(eSelect.name, eSelect);
+      Vue.component(eRadio.name, eRadio);
+      Vue.component(eCheckbox.name, eCheckbox);
+      Vue.component(eTextarea.name, eTextarea);
       Vue.component(eButton.name, eButton);
       Vue.component(eIcon.name, eIcon);
       Vue.component(ePicture.name, ePicture);
       Vue.component(eHeading.name, eHeading);
       Vue.component(ePanel.name, ePanel);
       Vue.component(eProgress.name, eProgress);
-      Vue.component(eInput.name, eInput);
-      Vue.component(eCheckbox.name, eCheckbox);
-      Vue.component(eLabel.name, eLabel);
-      Vue.component(eTextarea.name, eTextarea);
-      Vue.component(eRadio.name, eRadio);
       Vue.component(cModal.name, cModal);
       Vue.component(cModalHeader01.name, cModalHeader01);
-      Vue.component(eSelect.name, eSelect);
       Vue.component(lDefault.name, lDefault);
       Vue.component(eLink.name, eLink);
       Vue.component(cBreadcrumb.name, cBreadcrumb);
