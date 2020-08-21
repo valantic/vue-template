7e9235041ad481a3651146c84451ca028295c8c3
Patric Eberle <patric.eberle@cec.valantic.com>
2020-08-21 15:11:15 +0200

    Adds a new pimcore directive

---
+++ src/directives/pimcore.js
@@ -0,0 +1,14 @@
+/**
+ * Directive to apply pimcore generator attributes.
+ */
+export default {
+  name: 'pimcore',
+
+  // bind(el, binding) {
+
+  // },
+
+  // update(el, binding) {
+
+  // },
+};
