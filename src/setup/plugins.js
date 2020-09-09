984f7c337063fcdeb90c2564e47a4e1471082e3a
Patric Eberle <patric.eberle@cec.valantic.com>
2020-09-09 11:05:07 +0200

    Refactors import paths of plugins

--- src/setup/plugins.js
+++ src/setup/plugins.js
@@ -2,14 +2,14 @@
 import Vue from 'vue';
 import VModal from 'vue-js-modal';
 import PortalVue from 'portal-vue';
-import VueAxios from '../plugins/axios';
-import VueBemCn from '../plugins/vue-bem-cn';
-import resizeEnd from '../plugins/resize-end';
-import eventBus from '../plugins/event-bus';
-import viewport from '../plugins/viewport';
-import modal from '../plugins/modalStack';
-import api from '../plugins/api';
-// import dayjs from '../plugins/dayjs';
+import VueAxios from '@/plugins/axios';
+import VueBemCn from '@/plugins/vue-bem-cn';
+import resizeEnd from '@/plugins/resize-end';
+import eventBus from '@/plugins/event-bus';
+import viewport from '@/plugins/viewport';
+import modal from '@/plugins/modalStack';
+import api from '@/plugins/api';
+// import dayjs from '@/plugins/dayjs';
 
 // Self executing
 
