331639d09c917e8a93f12a5c99d084398b9cf8b7
Patric Eberle <patric.eberle@cec.valantic.com>
2020-11-06 11:06:22 +0100

    Updates documentation

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,9 @@
-module.exports = {
+module.exports = { // @see https://webpack.js.org/loaders/postcss-loader/#examples-of-config-files
   // Browser config is fetched from package.json:browserslist
   map: false,
   plugins: [
     'autoprefixer',
-    ['cssnano', {
+    ['cssnano', { // @see https://cssnano.co/docs/optimisations
       preset: [
         'default',
         {
