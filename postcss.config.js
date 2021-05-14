ab933f04f4a3570a10f7977ab6381dcdda547b00
Patric Eberle <patric.eberle@cec.valantic.com>
2021-05-14 08:26:44 +0200

    Updates PostCSS configuration

--- postcss.config.js
+++ postcss.config.js
@@ -2,12 +2,14 @@ module.exports = { // @see https://webpack.js.org/loaders/postcss-loader/#exampl
   // Browser config is fetched from package.json:browserslist
   map: false,
   plugins: [
-    'autoprefixer',
-    ['cssnano', { // @see https://cssnano.co/docs/optimisations
+    ['autoprefixer', {
+      grid: "no-autoplace",
+    }],
+    ['cssnano', {
       preset: [
         'default',
         {
-          calc: false,
+          calc: false, // The precision of SCSS calculations was to low, so we decided to keep calc() in the output to increase the precision.
         },
       ],
     }],
