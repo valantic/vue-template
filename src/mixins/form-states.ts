bcf4cb2821a8ec19f8a6a22c22ec9232ebbaff91
Patric Eberle <patric.eberle@cec.valantic.com>
2021-08-25 15:53:38 +0200

    #157: changes type definition for isChecked on form-states

--- src/mixins/form-states.ts
+++ src/mixins/form-states.ts
@@ -31,7 +31,7 @@ export interface IFormStates {
   stateModifiers: ComputedRef<IStateModifiers>;
   stateIcon: ComputedRef<string>;
   hasDefaultState: ComputedRef<boolean>;
-  isChecked: WritableComputedRef<boolean>;
+  isChecked: ComputedRef<boolean>;
 }
 
 export default (defaultValue: string = ''): IFormStates => {
