6e5f0b64a541d0a67fc721922a84cf78bf177fdf
Jan Widmer <jan.widmer@cec.valantic.com>
2021-08-30 15:14:14 +0200

    #157 adds initial data logic again

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,15 +1,18 @@
 import { createDirectStore, ModulesImpl } from 'direct-vuex';
 import getUrlParameter from '@/helpers/get-url-parameter';
+import { IMessage } from '@/types/c-notification';
 import session from './modules/session';
 import notification from './modules/notification';
 import breadcrumb from './modules/breadcrumb';
 
+interface IModules {
+  session: ModulesImpl;
+  breadcrumb: ModulesImpl;
+  notification: ModulesImpl;
+}
+
 export interface IStore {
-  modules: {
-    session: ModulesImpl;
-    breadcrumb: ModulesImpl;
-    notification: ModulesImpl;
-  }
+  modules: IModules;
 }
 
 interface IAction {
@@ -30,6 +33,9 @@ declare global {
   interface Window { initialData: IInitialData; }
 }
 
+const data: IInitialData = window.initialData || {};
+let initialDataMessages: IMessage[] = [];
+
 const {
   store,
   rootActionContext,
@@ -43,6 +49,41 @@ const {
     breadcrumb,
   }
 });
+
+// Set initial data
+Object.keys(data).forEach((action) => {
+  const { messages } = data[action] || {};
+
+  const storeModule = action.split('/')[0] as keyof IModules;
+  const moduleAction = action.split('/')[1];
+
+  // NOTE: We moved away from JSON again, since it was too picky about special characters in the to be parsed string.
+  // @ts-ignore
+  store.dispatch[storeModule][moduleAction](data[action]);
+
+  if (Array.isArray(messages)) {
+    initialDataMessages = initialDataMessages.concat(messages);
+  }
+});
+
+if (initialDataMessages.length) {
+  setTimeout(() => { // Make sure all general imports did run before.
+    initialDataMessages.forEach((message) => {
+      store.commit.notification.pushNotification({ message });
+    });
+  });
+}
+
+// Set theme according to url in development. e.g. ?theme=01
+if (process.env.NODE_ENV !== 'production') {
+  const themeId = getUrlParameter('theme');
+
+  if (themeId) {
+    store.commit.session.setTheme(themeId);
+  }
+}
+
+window.initialData = {};
 
 // Export the direct-store instead of the classic Vuex store.
 export default store;
