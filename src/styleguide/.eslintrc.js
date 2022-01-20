ca317df847d45b2b420311e313208eff9c485764
Patric Eberle <patric.eberle@cec.valantic.com>
2022-01-20 16:15:11 +0100

    Disables additional ESLint rules for styleguide context, replaces default code example with hint to prevent console errors if nothing is maintained

--- src/styleguide/.eslintrc.js
+++ src/styleguide/.eslintrc.js
@@ -2,5 +2,6 @@ module.exports = {
   rules: {
     'vue/no-bare-strings-in-template': ['off'],
     'vue/multi-word-component-names': ['off'],
+    'vue/match-component-file-name': ['off'],
   },
 };
