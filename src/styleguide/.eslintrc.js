dbaf9d5f09fac50b86f451131c10dd113d92b922
Patric Eberle <patric.eberle@cec.valantic.com>
2022-01-20 14:07:19 +0100

    Extends ESLint rules for styleguide folder

--- src/styleguide/.eslintrc.js
+++ src/styleguide/.eslintrc.js
@@ -1,5 +1,6 @@
 module.exports = {
   rules: {
-    'vue/no-bare-strings-in-template': ['off']
+    'vue/no-bare-strings-in-template': ['off'],
+    'vue/multi-word-component-names': ['off'],
   },
 };
