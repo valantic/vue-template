dfaa43ba66a58bcef533022426d20e2e703071ac
Michael Blesi <michael@blesi.ch>
2022-11-02 12:57:26 +0100

    adds msw for request mocking.

--- src/setup/styleguide.options.ts
+++ src/setup/styleguide.options.ts
@@ -2,6 +2,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import app from '@/styleguide/components/app.vue';
 import styleguideRoutes from './styleguide.routes';
+import '@/styleguide/mock-data/api/browser';
 
 // In development and on styleguide build, include router and example pages
 
