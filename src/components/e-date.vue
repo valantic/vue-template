6b2a16cf7ea23a07702961eb2320ab4586daa709
BeneRichi <benedict.ritschel@cec.valantic.com>
2023-01-10 16:25:51 +0100

    #131 Add existing components + scss and helper files

---
+++ src/components/e-date.vue
@@ -0,0 +1,326 @@
+<template>
+  <label
+    :style="style"
+    :class="b(modifiers)"
+    :title="format"
+  >
+    <span :class="b('label-text')">{{ label }}</span>
+    <input
+      v-model.lazy.trim="dateString"
+      :class="b('input')"
+      :maxlength="format.length"
+      :name="name"
+      @keypress.enter="onEnter"
+      @focus="onFocus"
+      @blur="onBlur"
+    >
+    <e-icon
+      :class="b('icon')"
+      icon="i-date-picker"
+      width="22"
+      height="22"
+    />
+  </label>
+</template>
+
+<script>
+  import getLocalizedDateFormat from '@/helpers/get-localized-date-format';
+
+  /**
+   * Renders a date styled input.
+   */
+  export default {
+    name: 'e-date',
+
+    // components: {},
+    // mixins: [],
+
+    model: {
+      /**
+       * Changes v-model behavior and use 'change' instead of 'value' as prop.
+       * Avoids conflict with default value attribute.
+       */
+      prop: 'value',
+      event: 'change',
+    },
+
+    props: {
+      /**
+       * Allows to set the accepted input format.
+       */
+      format: {
+        type: String,
+        default: () => getLocalizedDateFormat(),
+      },
+
+      /**
+       * Allows to invert styles on darker backgrounds.
+       */
+      negative: {
+        type: Boolean,
+        default: false,
+      },
+
+      /**
+       * The current value.
+       */
+      value: {
+        type: Date,
+        default: () => new Date(),
+      },
+
+      /**
+       * The label and placeholder value of the field.
+       * The placeholder attribute is required to make floating labels work with native CSS (ex. IE11).
+       */
+      label: {
+        type: String,
+        default() {
+          return this.$t('e-date.defaultLabel');
+        },
+      },
+
+      /**
+       * Adds name attribute
+       */
+      name: {
+        type: String,
+        required: true,
+      },
+    },
+    data() {
+      return {
+        /**
+         * The actual, valid value of the component (as Day.js Object).
+         */
+        dateValue: this.$dayjs(this.value),
+
+        /**
+         * The display and editable value of the input field.
+         */
+        textValue: '',
+
+        /**
+         * Holds the validity state of the component.
+         */
+        invalid: false,
+      };
+    },
+
+    computed: {
+      /**
+       * Returns the components BEM modifiers.
+       *
+       * @returns {Object}
+       */
+      modifiers() {
+        return {
+          invalid: this.invalid,
+          negative: this.negative,
+        };
+      },
+
+      /**
+       * Returns dynamic styles for the component.
+       *
+       * @returns {Object}
+       */
+      style() {
+        return {
+          '--e-date-format-length': `${this.format.length}ch`,
+        };
+      },
+
+      /**
+       * Calculates the supported input format patterns.
+       *
+       * @returns {Array.<String>}
+       */
+      inputFormats() {
+        const shortFormat = this.format
+          .replace('DD', 'D')
+          .replace('MM', 'M')
+          .replace('YYYY', 'YY');
+
+        return [this.format, shortFormat];
+      },
+
+      /**
+       * Validates, saves and returns the editable string value of the input field.
+       *
+       * @returns {String}
+       */
+      dateString: {
+        set(value) {
+          const date = this.$dayjs(value, this.inputFormats);
+
+          this.invalid = !date.isValid();
+
+          if (this.invalid) {
+            this.textValue = value;
+          } else {
+            this.dateValue = date;
+          }
+        },
+        get() {
+          return this.invalid
+            ? this.textValue
+            : this.dateValue.format(this.format);
+        },
+      },
+    },
+    watch: {
+      /**
+       * Reset the invalid state when the date updates (most likely from the date field).
+       *
+       * @param {Object} newValue - The updated value.
+       * @param {Object} oldValue - The previous value.
+       */
+      dateValue(newValue, oldValue) {
+        if (newValue.toDate().getTime() !== oldValue.toDate().getTime()) {
+          this.invalid = false;
+
+          /**
+           * Sends the current value to the parent.
+           *
+           * @event change
+           *
+           * @property {Date} date - The current date value.
+           */
+          this.$emit('change', this.dateValue.toDate());
+        }
+      },
+
+      /**
+       * Handles updates on the value prop.
+       *
+       * @param {Date} date - The new date value.
+       */
+      value(date) {
+        this.dateValue = this.$dayjs(date);
+      },
+    },
+
+    // beforeCreate() {},
+    // created() {},
+    // beforeMount() {},
+    // mounted() {},
+    // beforeUpdate() {},
+    // updated() {},
+    // activated() {},
+    // deactivated() {},
+    // beforeDestroy() {},
+    // destroyed() {},
+
+    methods: {
+      /**
+       * Event handler for 'enter' keypress on the input.
+       *
+       * @param {Event} event - The original DOM event.
+       */
+      onEnter(event) {
+        event.preventDefault(); // Prevents auto submit of form.
+        event.target.blur(); // Triggers update.
+      },
+
+      /**
+       * Event handler for the focus event of the input.
+       *
+       * @param {Event} event - The original DOM event.
+       */
+      onFocus(event) {
+        /**
+         * Forwards the focus event to the parent component.
+         *
+         * @event focus
+         *
+         * @property {Event} event - The original DOM event.
+         */
+        this.$emit('focus', event);
+      },
+
+      /**
+       * Event handler for the blur event of the input.
+       *
+       * @param {Event} event - The original DOM event.
+       */
+      onBlur(event) {
+        /**
+         * Forwards the blur event to the parent component.
+         *
+         * @event blur
+         *
+         * @property {Event} event - The original DOM event.
+         */
+        this.$emit('blur', event);
+      },
+    },
+    // render() {},
+  };
+</script>
+
+<style lang="scss">
+  @use '../setup/scss/variables' ;
+  @use '../setup/scss/mixins';
+  @use '../setup/scss/extends';
+  @use '../setup/scss/functions';
+
+  .e-date {
+    $height: 48px;
+    $border: 1px;
+
+    display: flex;
+    align-items: center;
+    padding: rem(0 variables.$spacing--25); // Equalizes vertical centering
+    border: $border solid transparent;
+    border-radius: 99999px; // Force half circles.
+    background: variables.$color-grayscale--600;
+
+    @include mixins.media(lg) {
+      @include mixins.font(variables.$font-size--14, 18px);
+    }
+
+    &__label-text {
+      grid-area: label;
+      align-self: center;
+      color: variables.$color-primary--1;
+    }
+
+    &__input,
+    &__input-spacer {
+      margin: rem(0 variables.$spacing--5);
+      color: variables.$color-grayscale--200;
+    }
+
+    &__input {
+      @extend %input-reset;
+
+      grid-area: input;
+      width: 14ch; // 8 letters + 2 dividers + some, because IE11 does not count outer spacing of 0
+      height: rem($height - (2 * $border) - 1px);
+      margin-bottom: 1px; // Improves vertical centering.
+
+      @supports (display: inline-grid) {
+        width: var(--e-date-format-length, 10ch);
+      }
+    }
+
+    &__icon {
+      grid-area: icon;
+      margin-left: auto;
+
+      img {
+        width: rem(22px);
+        height: rem(22px);
+      }
+    }
+
+    &--negative {
+      background: variables.$color-grayscale--600;
+    }
+
+    &--invalid {
+      border-color: variables.$color-status--danger;
+    }
+  }
+</style>
