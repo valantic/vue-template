1a00ebc4f2a528fee475220788a686be081c888a
Benjamin Joham <benjamin.joham@cec.valantic.com>
2023-04-14 11:10:37 +0200

    #237 create video element for youtube and vimeo add route to styleguide navigation

---
+++ src/elements/e-video.vue
@@ -0,0 +1,201 @@
+<template>
+  <!-- CONFIG
+    baseUrl:
+      - vimeo: "https://player.vimeo.com/video/
+      - youtube: "https://www.youtube.com/embed/"
+    controls:
+      - youtube disables in src with ?controls=0
+      - vimeo not able to disable controls
+    size:
+      - width & height can either be responsive or hard coded
+      - youtube standard -> width="560", height="315" -->
+  <div :class="b()">
+    <!-- <div style="padding: 56.25% 0 0 0; position: relative"> -->
+    <!-- style="position: absolute; top: 0; left: 0; width: 100%; height: 100%" -->
+    <iframe
+      :class="b('iframe')"
+      :src="mapSrc"
+      :allowfullscreen="allowfullscreen"
+      :frameborder="frameborder"
+      :width="width"
+      :height="height"
+    ></iframe>
+    <!-- </div> -->
+  </div>
+</template>
+
+<script lang="ts">
+  import { PropType, defineComponent } from 'vue';
+
+  const videoUrlSources = {
+    youtube: 'https://www.youtube.com',
+    vimeo: 'https://vimeo.com/',
+  };
+
+  const playerUrlSources = {
+    youtube: 'https://www.youtube.com/embed/',
+    vimeo: 'https://player.vimeo.com/video/',
+  };
+
+  export default defineComponent({
+    name: 'e-video',
+    status: 0, // TODO: remove when component was prepared for current project.
+
+    // components: {},
+
+    props: {
+      /**
+       * Allows the user to watch the video in fullscreen
+       */
+      allowfullscreen: {
+        type: Boolean,
+        default: false,
+      },
+
+      /**
+       * Specifies the source of the video
+       *
+       * We can decide between youtube and vimeo
+       */
+      source: {
+        type: String as PropType<'youtube' | 'vimeo'>,
+        required: true,
+        validator(value: string) {
+          return Object.keys(playerUrlSources).includes(value);
+        },
+      },
+
+      /**
+       * Allows to add/remoe the border around the player
+       *
+       * When set to 0 the border is completely removed
+       *
+       * Default from iframe is 2
+       */
+      frameborder: {
+        type: String,
+        default: '2',
+      },
+
+      /**
+       * Accepts a valid url from a youtube or vimeo video
+       */
+      videoUrl: {
+        type: String,
+        default: null,
+      // validate against different sources
+      // validator: (value: string) => playerSources[this.source].includes(value)
+      // validator: (value: string) => {
+      //   return Object.values(videoUrlSources).includes(value);
+      // }
+      },
+
+      /**
+       * Accepts an ID from a youtube or vimeo video
+       */
+      videoId: {
+        type: String,
+        default: null,
+      },
+
+      /**
+       * Allows to set the video player width.
+       */
+      width: {
+        type: [String, Number],
+        default: 560,
+      },
+
+      /**
+       * Allows to set the video player height.
+       */
+      height: {
+        type: [String, Number],
+        default: 315,
+      },
+
+    /**
+     * Defines the players size to be responsive 100% width to the parent container
+     */
+    // responsive: {
+    //   type: Boolean,
+    //   default: null
+    // }
+    },
+
+    // emits: {},
+    // setup(): Setup {},
+
+    computed: {
+      mapSrc(): string | undefined {
+        const playerBaseUrl = playerUrlSources[this.source];
+
+        if (!(this.videoId || this.videoUrl)) {
+          // eslint-disable-next-line no-console
+          console.error("Neither 'videoId' or 'videoUrl' were defined", this.$el);
+
+          return undefined;
+        }
+
+        if (this.videoUrl && !this.videoId) {
+          if (this.source === 'youtube') {
+            /**
+             * Extract id from video url
+             */
+            const id = this.videoUrl.substring(
+              this.videoUrl.indexOf('v=') + 2,
+              this.videoUrl.indexOf('&')
+            );
+
+            return `${playerBaseUrl}${id}`;
+          }
+
+          const id = this.videoUrl.substring(
+            this.videoUrl.indexOf(videoUrlSources[this.source])
+              + videoUrlSources[this.source].length,
+            this.videoUrl.length
+          );
+
+          return `${playerBaseUrl}${id}`;
+        }
+
+        if (this.videoId.includes(videoUrlSources[this.source])) {
+          // eslint-disable-next-line no-console
+          console.error('video-id can not be part of a URL');
+
+          return undefined;
+        }
+
+        return `${playerBaseUrl}${this.videoId}`;
+      },
+    },
+
+  // computed: {},
+  // watch: {},
+
+  // beforeCreate() {},
+  // created() {},
+  // beforeMount() {},
+  // mounted() {},
+  // beforeUpdate() {},
+  // updated() {},
+  // activated() {},
+  // deactivated() {},
+  // beforeUnmount() {},
+  // unmounted() {},
+
+  // methods: {},
+  // render() {},v
+  });
+</script>
+
+<style lang="scss">
+@use '../setup/scss/variables';
+
+.e-video {
+  &__iframe {
+    display: flex;
+    margin: variables.$spacing--20 auto;
+  }
+}
+</style>
---
+++ src/styleguide/routes/r-video.vue
@@ -0,0 +1,70 @@
+<template>
+  <div :class="b()">
+    <!-- youtube example with url -->
+    <e-video
+      source="youtube"
+      video-url="https://www.youtube.com/watch?v=WYs_Y4D3NUs&ab_channel=valantic"
+    />
+    <!-- youtube example with id -->
+    <e-video source="youtube" video-id="WYs_Y4D3NUs" />
+
+    <!-- vimeo example with url -->
+    <e-video source="vimeo" video-url="https://vimeo.com/264037633" />
+
+    <!-- vimeo example with id -->
+    <e-video source="vimeo" video-id="264037633" />
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent } from 'vue';
+
+  import eVideo from '@/elements/e-video.vue';
+
+  /**
+   * A component that integrates the e-picture for testing its functionality and passing properties.
+   */
+  export default defineComponent({
+    name: 'r-pictures',
+
+    components: {
+      eVideo,
+    },
+
+  // props: {}, // emits: {},
+
+  // setup(): Setup {},
+
+  // computed: {},
+  // watch: {},
+
+  // beforeCreate() {},
+  // created() {},
+  // beforeMount() {},
+  // mounted() {},
+  // beforeUpdate() {},
+  // updated() {},
+  // activated() {},
+  // deactivated() {},
+  // beforeUnmount() {},
+  // unmounted() {},
+
+  // methods: {},
+  // render() {},
+  });
+</script>
+
+<style lang="scss">
+@use '../../setup/scss/variables';
+
+.r-video {
+  &__form {
+    display: flex;
+  }
+
+  .e-input {
+    flex-grow: 1;
+    margin-right: variables.$spacing--20;
+  }
+}
+</style>
