05919e7ddf479a3ce630f18874e961a444954903
Mathias Ober <mathias.ober@cec.valantic.com>
2024-05-23 10:31:37 +0200

    #350: update vite to version 5 (#376)
    * #350: update vite version
    * #350: update packages
    * #350: update husky and jsdom

--- src/setup/ssr-components.ts
+++ src/setup/ssr-components.ts
@@ -14,7 +14,7 @@ const plugin: Plugin = {
     ];
 
     components.forEach((component) => {
-      app.component(component.name, component);
+      app.component(component.name || '', component);
     });
   },
 };
