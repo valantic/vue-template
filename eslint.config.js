86194f5f95fe1d0b69a834cc5b413f51ea270c63
Mathias Ober <mathias.ober@cec.valantic.com>
2024-09-16 11:22:49 +0200

    update config for eslint

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,14 @@
-import typescriptParser from '@typescript-eslint/parser';
+import eslint from '@eslint/js';
 import eslintConfigPrettier from 'eslint-config-prettier';
 import eslintConfigValantic from 'eslint-config-valantic';
-import eslintConfigTypescript from '@vue/eslint-config-typescript';
-import eslintPluginStorybook from 'eslint-plugin-storybook';
-import globals from 'globals';
+import tseslint from 'typescript-eslint';
 
 // https://eslint.org/docs/user-guide/configuring
-export default [
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommended,
+  eslintConfigValantic,
+  eslintConfigPrettier,
   {
     files: ['*.js', '*.ts', '*.vue'],
     ignores: [
@@ -20,21 +22,10 @@ export default [
       '!.storybook',
     ],
     languageOptions: {
-      parser: typescriptParser,
       globals: {
-        ...globals.browser,
-      },
-      parserOptions: {
-        requireConfigFile: false,
-        // Fixes an exception on sub folder eslintrc.js files
-        // Don't use on upper scope as documented by ESLint. The Vue plugin already defines itself as parser.
-        sourceType: 'module',
+        google: 'readonly', // Fixes `no-undef` for `google` namespace. (types are loaded by `@types/google.maps`)
       },
     },
-    ...eslintConfigValantic,
-    eslintConfigTypescript,
-    eslintConfigPrettier,
-    eslintPluginStorybook,
     // Uses eslint-import-resolver-webpack
     settings: {
       'import/resolver': {
@@ -43,58 +34,40 @@ export default [
         },
       },
     },
-    globals: {
-      google: 'readonly', // Fixes `no-undef` for `google` namespace. (types are loaded by `@types/google.maps`)
-    },
     // add your custom rules here
     rules: {
-      'vue/no-unsupported-features': [
-        'error',
-        {
-          version: '>= 3.2',
-        },
-      ],
-
       // TODO: Fix and enable again
       'import/extensions': 'off',
     },
-    overrides: [
-      {
-        files: ['tests/**/*.*'],
-        env: {
-          jest: true,
-        },
-        rules: {
-          'vue/require-name-property': 'off',
-        },
-      },
-      {
-        files: ['blueprints/**/*.*'],
-        env: {
-          jest: true, // Is required because of unit test blueprint.
-        },
-        rules: {
-          // Allow unused variables in blueprints.
-          '@typescript-eslint/no-unused-vars': 'off',
-          // Allow empty interfaces in blueprints.
-          '@typescript-eslint/no-empty-interface': 'off',
-          '@typescript-eslint/explicit-function-return-type': 'off',
-        },
-      },
-      {
-        files: ['src/styleguide/**/*.*'],
-        rules: {
-          'vue/no-bare-strings-in-template': 'off',
-          'vue/multi-word-component-names': 'off',
-          'vue/match-component-file-name': 'off',
-        },
-      },
-      {
-        files: ['src/stories/**/*.*'],
-        rules: {
-          '@typescript-eslint/explicit-function-return-type': 'off',
-        },
-      },
-    ],
   },
-];
+  {
+    files: ['tests/**/*.*'],
+    rules: {
+      'vue/require-name-property': 'off',
+    },
+  },
+  {
+    files: ['blueprints/**/*.*'],
+    rules: {
+      // Allow unused variables in blueprints.
+      '@typescript-eslint/no-unused-vars': 'off',
+      // Allow empty interfaces in blueprints.
+      '@typescript-eslint/no-empty-interface': 'off',
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+  },
+  {
+    files: ['src/styleguide/**/*.*'],
+    rules: {
+      'vue/no-bare-strings-in-template': 'off',
+      'vue/multi-word-component-names': 'off',
+      'vue/match-component-file-name': 'off',
+    },
+  },
+  {
+    files: ['src/stories/**/*.*'],
+    rules: {
+      '@typescript-eslint/explicit-function-return-type': 'off',
+    },
+  }
+);
