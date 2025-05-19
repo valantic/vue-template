631c7bee176ba3d417d53ea1069a42ad894313ff
Mathias Ober <mathias.ober@cec.valantic.com>
2025-05-19 16:08:50 +0200

    Add floating-vue as tooltip plugin

---
+++ src/plugins/tooltip.ts
@@ -0,0 +1,6 @@
+import FloatingVue from 'floating-vue';
+import 'floating-vue/dist/style.css';
+
+export default { plugin: FloatingVue };
+
+// See https://floating-vue.starpad.dev/guide/component for further documentation.
