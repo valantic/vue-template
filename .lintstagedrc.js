99426f47b55bbed7bf189e871daf4095c3777df4
Mathias Ober <mathias.ober@cec.valantic.com>
2026-04-13 09:28:20 +0200

    #396: update prettier hook to remove duplicate runs

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,7 +1,13 @@
-// NOTE: 'concurrent' had to be disabled to allow prettier to apply code changes before linting.
 export default {
-  '*.{js,ts,vue}': ['npx prettier --write', () => 'vue-tsc'],
-  '*.{css,vue,scss}': ['npx prettier --write', 'stylelint --config .stylelintrc.fix.js --fix'],
-  // Make sure the selector does not overlap to prevent race conditions!
-  '!*.{js,ts,vue,css,scss}': ['npx prettier --ignore-unknown --write'],
+  // 1. Script files: Prettier first, then ESLint (fixes applied sequentially)
+  '*.{js,ts}': ['prettier --write', 'eslint --fix'],
+
+  // 2. Style files: Prettier first, then Stylelint (fixes applied sequentially)
+  '*.{css,scss}': ['prettier --write', 'stylelint --config .stylelintrc.fix.js --fix'],
+
+  // 3. Vue files: Prettier, then ESLint, then Stylelint (all in one go)
+  '*.vue': ['prettier --write', 'eslint --fix', 'stylelint --config .stylelintrc.fix.js --fix'],
+
+  // 4. Everything else: Just Prettier
+  '!*.{js,ts,vue,css,scss}': ['prettier --ignore-unknown --write'],
 };
